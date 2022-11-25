73e6000e1129633fbdd3633e116559199cc612b6
Harold Varde <harvardii15@gmail.com>
2022-11-25 18:34:04 +0800

    gsap demo

--- main.js
+++ main.js
@@ -7,10 +7,18 @@ const activeNav = document.querySelector(".active-nav");
 
 items.forEach((item) => {
   item.addEventListener("click", (e) => {
+    // Make items blue
     gsap.to(items, { color: "#222" });
-
     if (document.activeElement === e.target) {
       gsap.to(item, { color: "rgb(75, 20, 177)" });
     }
+
+    // Move line
+    const state = Flip.getState(activeNav);
+    item.appendChild(activeNav);
+    Flip.from(state, {
+      duration: 0.5,
+      absolute: true,
+    });
   });
 });
